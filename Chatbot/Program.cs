373b3cbf056c6e0825ca83bc4c0a1cda18079fe2
BRNFarias <bfarias088@gmail.com>
2025-05-22 00:07:54 -0300

    começo de tudo

--- Chatbot/Program.cs
+++ Chatbot/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Faça aqui a abertura do seu chamado!");
+Console.WriteLine("Digite o seu nome: ");
+string nome = Console.ReadLine() ?? "";
+Console.WriteLine("Digite o seu e-mail: ");
+string email = Console.ReadLine() ?? "";
+Console.WriteLine("Digite o seu telefone: ");
+string telefone = Console.ReadLine() ?? "";
+Console.WriteLine("\n1 - abertura de chamado \n2 - Falar com suporte \n3 - sair");
+string opcao = Console.ReadLine() ?? "";
+
+switch (opcao)
+{
+    case "1":
+        Console.WriteLine("Abrindo chamado...");
+        break;
+    case "2":
+        Console.WriteLine("Transferindo para o suporte...");
+        break;
+    case "3":
+        Console.WriteLine("Saindo do sistema...");
+        break;
+    default:
+        Console.WriteLine("Opção inválida.");
+        return;
+}
